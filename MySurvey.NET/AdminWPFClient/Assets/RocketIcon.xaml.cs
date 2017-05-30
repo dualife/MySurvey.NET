41e60247e36e412cd4944e2a51d4ef10397cb531
cyril gosselin <gossel_c@outlook.fr>
2017-05-30 22:13:39 +0200

    Found icons

---
+++ MySurvey.NET/AdminWPFClient/Assets/RocketIcon.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace AdminWPFClient.Assets
+{
+    /// <summary>
+    /// Logique d'interaction pour GoToFormUrlIcon.xaml
+    /// </summary>
+    public partial class RocketIcon : UserControl
+    {
+        public RocketIcon()
+        {
+            InitializeComponent();
+        }
+    }
+}
